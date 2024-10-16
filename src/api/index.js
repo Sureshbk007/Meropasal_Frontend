537a1b9ffdfa947d88be95f7379f8d8916c064d8
Suresh BK <sureshbk.contact@gmail.com>
2024-10-16 09:07:08 +0545

    api update

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://meropasal-backend.onrender.com//api/v1",
 });
 
 API.interceptors.request.use(
